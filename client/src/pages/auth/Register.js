05ced5e87370616408adde9e30bb3b0467b08a6e
SyedAzhar <syed.moh.azhar.98@gmail.com>
2021-02-06 22:24:34 +0500

    Register Form creation

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const register =()=>{
+const Register =()=>{
+
+
+    const [email,setEmail]=useState();
+
+
+    const handleSubmit=()=>{
+        //this function will send request to firebase, so that firebase send email to user 
+
+
+    }
+
+
+
+
+
+
+    const registrationForm=()=><form onSubmit={handleSubmit}>
+
+        <input type="email"
+         className="form-control"
+         value={email}
+         onChange={e=>setEmail(e.target.value)}
+         autoFocus   />
+
+         <button type="submit" className="btn btn-raised">Register</button>
+
+    </form>
+
+
+
 
     return(
-    <div>
-        <h1>register page</h1>
+    <div className="container p-5">
+
+       <div className="row">
+           <div className=" col-md-6 offset-md-3">
+            <h1>Register</h1>
+             {registrationForm()}
+           </div>
+
+       </div>
+
+
     </div>
     )
 }
 
-export default register;
+export default Register;
