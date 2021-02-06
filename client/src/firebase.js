6a79ba322948235e8e0eb2b8f76153826a7eb7ea
SyedAzhar <syed.moh.azhar.98@gmail.com>
2021-02-06 19:24:02 +0500

    Firebase setup

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+ import * as firebase from 'firebase'
+ 
+ 
+ // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDs5WaqERXE7cNHH3MDAPCcDT83sL2WAWs",
+    authDomain: "ecommerce-ded37.firebaseapp.com",
+    projectId: "ecommerce-ded37",
+    storageBucket: "ecommerce-ded37.appspot.com",
+    messagingSenderId: "385733654747",
+    appId: "1:385733654747:web:b07efc18a6c4541da6f4fe"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //export functionality from firebase
+
+  export const auth = firebase.auth();
+  export const googleAuthProvider= new firebase.auth.googleAuthProvider(); //we create new instance of firebase.auth.googleprovider
+
